60171a7806baa55052aaeec3e4af65a63bff2ef8
Lucklyric <xinyao1@ualberta.ca>
2016-04-27 22:55:03 -0600

    1. Fix some minor changes

--- pages/contact.php
+++ pages/contact.php
@@ -1,10 +1,10 @@
 <?php
 
-$ownermail 	= 	'example@domain.com';
+$ownermail 	= 	'xinyao1@ualberta.ca';
 $from 		= 	$_POST['email'];
 $message 	= 	$_POST['message'];
 $name		= 	$_POST['name'];
-$subject 	= 	$name.' Sent a messgae from IMX contact form';
+$subject 	= 	$name.' Sent a messgae from UA webdoc contact form';
 
 
 mail($ownermail, $subject, $message, "From: $name <$from>\r\n");
@@ -14,7 +14,7 @@ mail($ownermail, $subject, $message, "From: $name <$from>\r\n");
 // mail to sender
 
 $to = $from;
-$message = 'You just sent a email using IMX Contact Form, the mail on the way to destination';
+$message = 'You just sent a email using UA webdoc Form, the mail on the way to destination';
 $subject = 'Mail Sent confirmation';
 
 mail($to, $subject, $message, "From: $name <$ownermail>\r\n");
